a087516d3471dec3562980830aabc7ef6f299cda
otasch <otasch@yahoo.com>
2015-07-29 11:42:00 +0200

    update

--- template/header.php
+++ template/header.php
@@ -3,6 +3,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title><?php echo $page['title'] . ' | ' . $site_title; ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
