2b2382e50b662dfb11009f81a1a1176124bdf864
otasch <otasch@yahoo.com>
2015-08-08 20:40:48 +0200

    edit footer

--- template/footer.php
+++ template/footer.php
@@ -2,8 +2,16 @@
 
 	<footer id="footer">
 		
-		<div class="container">
-			<div id="footer_soc">Potrazite nas na</div>
+		<div class="container-fluid">
+			<div id="footer_soc">
+				<span>Potrazite nas na</span> 
+				<div>
+					<img src="images/fb.png" alt="" class="img-responsive">
+					<img src="images/tw.png" alt="" class="img-responsive">
+					<img src="images/li.png" alt="" class="img-responsive">
+				</div>
+				<p class="clear"></p>
+			</div>
 			<div id="footer_contact">
 					<div>DOKI VET</div>
 					<div>Petra Drapsina 70, Coka</div>
