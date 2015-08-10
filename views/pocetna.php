5ca2417963216e66a8fe521fefe62f5f140aaac7
otasch <otasch@yahoo.com>
2015-08-10 18:39:01 +0200

    fb

--- views/pocetna.php
+++ views/pocetna.php
@@ -1,5 +1,5 @@
 <?php //Pocetna ?>
-<img src="images/naslovna.png" alt="doki-vet pregled" class="img-responsive">
+<img src="images/naslovna.png" alt="doki-vet pregled" class="img-responsive full-width">
 	
 	<h1 class="naslov">O nama</h1>
 	<div class="row" id="pocetna_text">
@@ -36,13 +36,19 @@
 	</div> <!-- row -->
 	<div class="row" id="foto_link">
 		<div class="col-md-4">
-			<img src="images/tmb1.png" alt="" class="img-responsive">
+			<div class="podnaslov">MALA PRAKSA</div>
+			<img src="images/tmb1.png" alt="" class="img-responsive full-width">
+			<div class="vise">Vise o temi</div>
 		</div>
 		<div class="col-md-4">
-			<img src="images/tmb2.png" alt="" class="img-responsive">
+			<div class="podnaslov">VELIKA PRAKSA</div>
+			<img src="images/tmb2.png" alt="" class="img-responsive full-width">
+			<div class="vise">Vise o temi</div>
 		</div>
 		<div class="col-md-4">
-			<img src="images/tmb3.png" alt="" class="img-responsive">
+			<div class="podnaslov">VESTI</div>
+			<img src="images/tmb3.png" alt="" class="img-responsive full-width">
+			<div class="vise">Vise o temi</div>
 		</div>
 	</div> <!--END row -->
 	<div id="drugi-o-nama">
@@ -52,20 +58,25 @@ Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lo
 		</div>
 		<div class="clear"></div>
 	</div><!--END #drugi-o-nama -->
-	<div class="row" id="text_link">
+	<div class="row row-eq-height" id="text_link">
 		<div class="col-md-4">
 			<h2>Blog</h2>
 			Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.
-Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod. Nonummy nibh euismod tincidunt ut laoreet.v
+Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod. Nonummy nibh euismod tincidunt ut laoreet.Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.
+Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.
+Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod. Nonummy nibh euismod tincidunt ut laoreet.Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.
+Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod. Nonummy nibh euismod tincidunt ut laoreet.
+Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.
+Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
 		</div>
 		<div class="col-md-4">
 			<h2>Pitali ste<br> - odgovaramo</h2>
 			Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.
 Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod. Nonummy nibh euismod tincidunt ut laoreet.
+Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.
+Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
 		</div>
 		<div class="col-md-4">
-			<h2>Facebook</h2>
-			Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.
-Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod. Nonummy nibh euismod tincidunt ut laoreet.
+			<div class="fb-page" data-width="500" data-href="https://www.facebook.com/facebook" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false" data-show-posts="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/facebook"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div></div>
 		</div>
 	</div> <!--END row -->
