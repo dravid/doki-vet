9c52652b6f46e8dbc636ae152c040539a965d759
otasch <otasch@yahoo.com>
2015-08-18 01:36:06 +0200

    o-nama

--- views/o-nama.php
+++ views/o-nama.php
@@ -1,15 +1,64 @@
 <?php //O nama ?>
-<div id="img-onama">
+<div id="img-sapa">
 	<picture>
 		<source 
 			media="(min-width: 1100px)" 
-			srcset="images/naslovna-lg.png">
+			srcset="images/o-nama-lg.png">
 		<source 
 			media="(min-width: 768px)" 
-			srcset="images/naslovna-md.png">
-		<img src="images/naslovna-sm.png" alt="doki-vet pregled" class="img-responsive full-width">
+			srcset="images/o-nama-md.png">
+		<img src="images/o-nama-sm.png" alt="doki-vet pregled" class="img-responsive full-width">
 	</picture>
 	<div id="sapa"></div>
-	<div> <h2><span>Mi pazimo</span><br> na zdravlje<br> Va&#353;eg ljubimca</h2></div>
+	<div> <h2><span>Poverenje</span><br> stru&#269;nost<br> posve&#263;enost</h2></div>
 </div>	
 	<h1 class="naslov">O nama</h1>
+	<div class="row">
+		<div class="col-sm-9">
+			<div class="row">
+				<div class="col-sm-6">
+				col-sm-6
+				</div>
+				<div class="col-sm-6">
+				col-sm-6
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">col-sm-12
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">Podnaslov
+				</div>
+				<div class="col-sm-4">col-sm-4
+				</div>
+				<div class="col-sm-4">col-sm-4
+				</div>
+				<div class="col-sm-4">col-sm-4
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-4">col-sm-4
+				</div>
+				<div class="col-sm-4">col-sm-4
+				</div>
+				<div class="col-sm-4">col-sm-4
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="col-sm-12">Podnaslov
+			</div>
+			<div class="col-sm-12">col-sm-12
+			</div>
+			<div class="col-sm-12">col-sm-12
+			</div>
+			<div class="col-sm-12">col-sm-12
+			</div>
+			<div class="col-sm-12">col-sm-12
+			</div>
+		</div>
+	
+	
+	
+	</div>
