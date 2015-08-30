3c76a8bcf3c65e639ae412f8533a80a3bd3a2afb
otasch <otasch@yahoo.com>
2015-08-31 01:40:48 +0200

    da

--- views/o-nama.php
+++ views/o-nama.php
@@ -13,49 +13,140 @@
 	<div> <h2><span>Poverenje</span><br> stru&#269;nost<br> posve&#263;enost</h2></div>
 </div>	
 	<h1 class="naslov">O nama</h1>
-	<div class="row">
+	<div class="row" id="about_content">
 		<div class="col-sm-9">
 			<div class="row">
 				<div class="col-sm-6">
-				col-sm-6
+				Veterinarska stanica "Doki-Vet" je osno-vana 2006. godine. Osnivač i vlasnik je dr.vet.med. Duško Petrović. Kao član udruženja veterinara male prakse Srbije, Duško Petrović je odlučio da delatnosti svoje stanice usmeri ka maloj kao i ka velikoj praksi. Veterinarska stanica "Doki-Vet" zapošjava četiri veterinara, dva veterinarska tehničara i jednog operatera.
 				</div>
 				<div class="col-sm-6">
-				col-sm-6
+				Veterinarska stanica "Doki-Vet" je osno-vana 2006. godine. Osnivač i vlasnik je dr.vet.med. Duško Petrović. Kao član udruženja veterinara male prakse Srbije, Duško Petrović je odlučio da delatnosti svoje stanice usmeri ka maloj kao i ka velikoj praksi. Veterinarska stanica "Doki-Vet" zapošjava četiri veterinara, dva veterinarska tehničara i jednog operatera.
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-sm-12">col-sm-12
+				<div class="col-sm-12">
+					<h1>Podnaslov</h1>
+					<p>Veterinarska stanica "Doki-Vet" je osno-vana 2006. godine. Osnivač i vlasnik je dr.vet.med. Duško Petrović. Kao član udruženja veterinara male prakse Srbije, Duško Petrović je odlučio da delatnosti svoje stanice usmeri ka maloj kao i ka velikoj praksi. Veterinarska stanica "Doki-Vet" zapošjava četiri veterinara, dva veterinarska tehničara i jednog operatera.</p>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-sm-12">Podnaslov
+			<div class="row nas_tim">
+				<div class="col-sm-12">
+					<h1>Na&#353; tim</h1>
 				</div>
-				<div class="col-sm-4">col-sm-4
+				<div class="col-sm-4">
+					<img src="images/round-staff.png" alt="Na&#353; tim" class="img-circle img-responsive">
+					<div>Ime i prezime</div>
+					<div>Zanimanje</div>
 				</div>
-				<div class="col-sm-4">col-sm-4
+				<div class="col-sm-4">
+					<img src="images/round-staff.png" alt="Na&#353; tim" class="img-circle img-responsive">
+					<div>Ime i prezime</div>
+					<div>Zanimanje</div>
 				</div>
-				<div class="col-sm-4">col-sm-4
+				<div class="col-sm-4">
+					<img src="images/round-staff.png" alt="Na&#353; tim" class="img-circle img-responsive">
+					<div>Ime i prezime</div>
+					<div>Zanimanje</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-sm-4">col-sm-4
+			<div class="row nas_tim">
+				<div class="col-sm-4">
+					<img src="images/round-staff.png" alt="Na&#353; tim" class="img-circle img-responsive">
+					<div>Ime i prezime</div>
+					<div>Zanimanje</div>
 				</div>
-				<div class="col-sm-4">col-sm-4
+				<div class="col-sm-4">
+					<img src="images/round-staff.png" alt="Na&#353; tim" class="img-circle img-responsive">
+					<div>Ime i prezime</div>
+					<div>Zanimanje</div>
 				</div>
-				<div class="col-sm-4">col-sm-4
+				<div class="col-sm-4">
+					<img src="images/round-staff.png" alt="Na&#353; tim" class="img-circle img-responsive">
+					<div>Ime i prezime</div>
+					<div>Zanimanje</div>
 				</div>
 			</div>
 		</div>
-		<div class="col-sm-3">
-			<div class="col-sm-12">Podnaslov
+		<div class="col-sm-3" id="about_gallery">
+			<div class="col-sm-12">
+				<h1>Galerija</h1>
+				<a href="#">Pogledaj galeriju</a>
 			</div>
-			<div class="col-sm-12">col-sm-12
+			<div class="col-xs-6 col-sm-12">
+				<a href="#">
+						
+					<picture>
+					
+						<source 
+							media="(min-width: 1100px)" 
+							srcset="images/7.png">
+						<source 
+							media="(min-width: 768px)" 
+							srcset="images/7-md.png">
+						<source 
+							media="(min-width: 450px)" 
+							srcset="images/7-200.png">
+						<img src="images/7-sm.png" alt="doki-vet galerija" class="img-responsive full-width">
+					</picture>
+					
+				</a>
 			</div>
-			<div class="col-sm-12">col-sm-12
+			<div class="col-xs-6 col-sm-12">
+				<a href="#">
+						
+					<picture>
+					
+						<source 
+							media="(min-width: 1100px)" 
+							srcset="images/6.png">
+						<source 
+							media="(min-width: 768px)" 
+							srcset="images/6-md.png">
+						<source 
+							media="(min-width: 450px)" 
+							srcset="images/6-200.png">
+						<img src="images/6-sm.png" alt="doki-vet galerija" class="img-responsive full-width">
+					</picture>
+					
+				</a>
 			</div>
-			<div class="col-sm-12">col-sm-12
+			<div class="col-xs-6 col-sm-12">
+				<a href="#">
+						
+					<picture>
+					
+						<source 
+							media="(min-width: 1100px)" 
+							srcset="images/8.png">
+						<source 
+							media="(min-width: 768px)" 
+							srcset="images/8-md.png">
+						<source 
+							media="(min-width: 450px)" 
+							srcset="images/8-200.png">
+						<img src="images/8-sm.png" alt="doki-vet galerija" class="img-responsive full-width">
+					</picture>
+					
+				</a>
 			</div>
-			<div class="col-sm-12">col-sm-12
+			<div class="col-xs-6 col-sm-12">
+				<a href="#">
+						
+					<picture>
+					
+						<source 
+							media="(min-width: 1100px)" 
+							srcset="images/9.png">
+						<source 
+							media="(min-width: 768px)" 
+							srcset="images/9-md.png">
+						<source 
+							media="(min-width: 450px)" 
+							srcset="images/9-200.png">
+						<img src="images/9-sm.png" alt="doki-vet galerija" class="img-responsive full-width">
+					</picture>
+					
+				</a>
 			</div>
 		</div>
 	
--- views/velika-praksa.php
+++ views/velika-praksa.php
@@ -1,2 +1,149 @@
 <?php //Velika praksa ?>
-<h1>Velika praksa</h1>
+<div id="img-sapa">
+	<picture>
+		<source 
+			media="(min-width: 1100px)" 
+			srcset="images/o-nama-lg.png">
+		<source 
+			media="(min-width: 768px)" 
+			srcset="images/o-nama-md.png">
+		<img src="images/o-nama-sm.png" alt="doki-vet pregled" class="img-responsive full-width">
+	</picture>
+	<div id="sapa"></div>
+	<div> <h2><span>Poverenje</span><br> stru&#269;nost<br> posve&#263;enost</h2></div>
+</div>
+	<h1 class="naslov">Velika praksa</h1>
+	<div class="row" id="mala_content">
+		<div class="col-sm-9">
+			<div class="row">
+				<div class="col-sm-6">
+				Veterinarska stanica "Doki-Vet" je osno-vana 2006. godine. Osnivač i vlasnik je dr.vet.med. Duško Petrović. Kao član udruženja veterinara male prakse Srbije, Duško Petrović je odlučio da delatnosti svoje stanice usmeri ka maloj kao i ka velikoj praksi. Veterinarska stanica "Doki-Vet" zapošjava četiri veterinara, dva veterinarska tehničara i jednog operatera.
+				</div>
+				<div class="col-sm-6">
+				Veterinarska stanica "Doki-Vet" je osno-vana 2006. godine. Osnivač i vlasnik je dr.vet.med. Duško Petrović. Kao član udruženja veterinara male prakse Srbije, Duško Petrović je odlučio da delatnosti svoje stanice usmeri ka maloj kao i ka velikoj praksi. Veterinarska stanica "Doki-Vet" zapošjava četiri veterinara, dva veterinarska tehničara i jednog operatera.
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<h1>Usluge</h1>
+					<p>Veterinarska stanica "Doki-Vet" je osno-vana 2006. godine. Osnivač i vlasnik je dr.vet.med. Duško Petrović. Kao član udruženja veterinara male prakse Srbije, Duško Petrović je odlučio da delatnosti svoje stanice usmeri ka maloj kao i ka velikoj praksi. Veterinarska stanica "Doki-Vet" zapošjava četiri veterinara, dva veterinarska tehničara i jednog operatera.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<h1>Usluga 1</h1>
+				</div>
+				<div class="col-sm-3">
+					<img src="images/round-staff.png" alt="Na&#353; tim" class="img-responsive">
+				</div>
+				<div class="col-sm-9">
+					Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. 
+				</div>
+				<div class="col-sm-12">
+					<h1>Usluga 1</h1>
+				</div>
+				<div class="col-sm-3">
+					<img src="images/round-staff.png" alt="Na&#353; tim" class="img-responsive">
+				</div>
+				<div class="col-sm-9">
+					Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. 
+				</div>
+				<div class="col-sm-12">
+					<h1>Usluga 1</h1>
+				</div>
+				<div class="col-sm-3">
+					<img src="images/round-staff.png" alt="Na&#353; tim" class="img-responsive">
+				</div>
+				<div class="col-sm-9">
+					Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. 
+				</div>
+				
+				
+			</div>
+		</div>
+		<div class="col-sm-3" id="about_gallery">
+			<div class="col-sm-12">
+				<h1>Galerija</h1>
+				<a href="#">Pogledaj galeriju</a>
+			</div>
+			<div class="col-xs-6 col-sm-12">
+				<a href="#">
+						
+					<picture>
+					
+						<source 
+							media="(min-width: 1100px)" 
+							srcset="images/7.png">
+						<source 
+							media="(min-width: 768px)" 
+							srcset="images/7-md.png">
+						<source 
+							media="(min-width: 450px)" 
+							srcset="images/7-200.png">
+						<img src="images/7-sm.png" alt="doki-vet galerija" class="img-responsive full-width">
+					</picture>
+					
+				</a>
+			</div>
+			<div class="col-xs-6 col-sm-12">
+				<a href="#">
+						
+					<picture>
+					
+						<source 
+							media="(min-width: 1100px)" 
+							srcset="images/6.png">
+						<source 
+							media="(min-width: 768px)" 
+							srcset="images/6-md.png">
+						<source 
+							media="(min-width: 450px)" 
+							srcset="images/6-200.png">
+						<img src="images/6-sm.png" alt="doki-vet galerija" class="img-responsive full-width">
+					</picture>
+					
+				</a>
+			</div>
+			<div class="col-xs-6 col-sm-12">
+				<a href="#">
+						
+					<picture>
+					
+						<source 
+							media="(min-width: 1100px)" 
+							srcset="images/8.png">
+						<source 
+							media="(min-width: 768px)" 
+							srcset="images/8-md.png">
+						<source 
+							media="(min-width: 450px)" 
+							srcset="images/8-200.png">
+						<img src="images/8-sm.png" alt="doki-vet galerija" class="img-responsive full-width">
+					</picture>
+					
+				</a>
+			</div>
+			<div class="col-xs-6 col-sm-12">
+				<a href="#">
+						
+					<picture>
+					
+						<source 
+							media="(min-width: 1100px)" 
+							srcset="images/9.png">
+						<source 
+							media="(min-width: 768px)" 
+							srcset="images/9-md.png">
+						<source 
+							media="(min-width: 450px)" 
+							srcset="images/9-200.png">
+						<img src="images/9-sm.png" alt="doki-vet galerija" class="img-responsive full-width">
+					</picture>
+					
+				</a>
+			</div>
+		</div>
+	
+	
+	
+	</div>
