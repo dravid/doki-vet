0c166697bda2f05765b6c50277fdb26b14a53fe2
otasch <otasch@yahoo.com>
2015-09-10 06:03:48 +0200

    ostale usluge

--- views/ostale-usluge.php
+++ views/ostale-usluge.php
@@ -17,16 +17,16 @@
 	</div>
 </div>
 <div class="row  text-justify" id="ostale_usluge_content">
-	<div class="col-sm-12">Usluga1</div>
-	<div class="col-sm-9">tekst</div>
-	<div class="col-sm-3">slika</div>
-	<div class="col-sm-12">Usluga2</div>
-	<div class="col-sm-9">tekst</div>
-	<div class="col-sm-3">slika</div>
-	<div class="col-sm-12">Usluga3</div>
-	<div class="col-sm-9">tekst</div>
-	<div class="col-sm-3">slika</div>
-	<div class="col-sm-12">Usluga4</div>
-	<div class="col-sm-9">tekst</div>
-	<div class="col-sm-3">slika</div>
+	<div class="col-sm-12"><h1>Usluga1</h1></div>
+	<div class="col-sm-9">Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. </div>
+	<div class="col-sm-3"><img src="images/round-staff.png" alt="Na&#353; tim" class="img-responsive"></div>
+	<div class="col-sm-12"><h1>Usluga2</h1></div>
+	<div class="col-sm-9">Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. </div>
+	<div class="col-sm-3"><img src="images/round-staff.png" alt="Na&#353; tim" class="img-responsive"></div>
+	<div class="col-sm-12"><h1>Usluga3</h1></div>
+	<div class="col-sm-9">Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. </div>
+	<div class="col-sm-3"><img src="images/round-staff.png" alt="Na&#353; tim" class="img-responsive"></div>
+	<div class="col-sm-12"><h1>Usluga4</h1></div>
+	<div class="col-sm-9">Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. </div>
+	<div class="col-sm-3"><img src="images/round-staff.png" alt="Na&#353; tim" class="img-responsive"></div>
 </div>
