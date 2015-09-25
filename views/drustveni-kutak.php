ff4ce185a0357298a38a81c94a7e6d68e984c507
otasch <otasch@yahoo.com>
2015-09-25 05:39:59 +0200

    mobile fix

--- views/drustveni-kutak.php
+++ views/drustveni-kutak.php
@@ -17,44 +17,103 @@
 	</div>
 </div>
 <h1 class="naslov">Doki Vet blog</h1>
-<div id="blog">
-	<div class="row">
-		<div class="col-sm-9">
-			<div class="row">
-				<div class="col-sm-12">
-					Naslov + autor
-				</div>
-				<div class="col-sm-12">
-					pasus
-				</div>
-				<div class="col-sm-6">
-					pasus
-				</div>
-				<div class="col-sm-6">
-					slika
-				</div>
-				<div class="col-sm-12">
-					pasus
-				</div>
-				<div class="col-sm-12">
-					naslov
-				</div>
-				<div class="col-sm-4">
-					Slika + tekst
-				</div>
-				<div class="col-sm-4">
-					Slika + tekst
-				</div>
-				<div class="col-sm-4">
-					Slika + tekst
-				</div>
-				<div class="col-sm-12">
-					Prijatelji
-				</div>
+
+<div class="row" id="blog">
+	<div class="col-sm-9">
+		<div class="row">
+			<div class="col-sm-12">
+				<h1>Naslov</h1> 
+				<h6>Autor:Petar Petrovi&#263;</h6>
 			</div>
 		</div>
-		<div class="col-sm-3">
-			Facebook + Pitali ste
+		<div class="row">
+			<div class="col-sm-12">
+				Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. 
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-8">
+				Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. 
+			</div>
+			<div class="col-sm-4">
+				<img src="images/round-staff.png" alt="Na&#353; tim" class="img-responsive">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12">
+				Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. 
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12">
+				<h1>Poslednji &#269;lanci</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4">
+				<img src="images/round-staff.png" alt="Na&#353; tim" class="img-responsive"> Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. 
+			</div>
+			<div class="col-sm-4">
+				<img src="images/round-staff.png" alt="Na&#353; tim" class="img-responsive"> Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. 
+			</div>
+			<div class="col-sm-4">
+				<img src="images/round-staff.png" alt="Na&#353; tim" class="img-responsive"> Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat. 
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12">
+				<h1>Prijatelji</h1>
+				<ul>
+					<li><a href="#">www.google.com</a></li>
+					<li><a href="#">www.google.com</a></li>
+					<li><a href="#">www.google.com</a></li>
+					<li><a href="#">www.google.com</a></li>
+					<li><a href="#">www.google.com</a></li>
+				</ul>
+			</div>
 		</div>
 	</div>
+	<div class="col-sm-3">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="fb-page" data-href="https://www.facebook.com/facebook" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/facebook"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div></div>
+			</div>
+		</div>
+		<div id="pitanja">
+			<div class="col-sm-12">
+				<h2>Pitali ste<br> - odgovaramo</h2>
+			
+			</div>
+			<div class="col-xs-6 col-sm-12">
+				<div>
+				<img src="images/pitalac.png" alt="pitanje" class="img-responsive" />
+				<h4>Moj ljubimac<br> ima buve</h4>
+				<div class="clear"></div>
+				</div>
+				Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat.
+			</div>
+			<div class="col-xs-6 col-sm-12">
+				<div>
+				<img src="images/dusan.png" alt="pitanje" class="img-responsive" />
+				<h5>Odgovara<br> dr vet. med. Du&#353;ko Petrovi&#263;</h5>
+				<div class="clear"></div>
+				</div>
+				Lorem ipsum dolor sit amet, consectetuer adi-piscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamco-rper suscipit lobortis nisl ut aliquip ex ea commodo con-sequat.
+			</div>
+		</div>
+	</div>
+	<picture id="dr-kucovic">
+					
+		<source 
+			media="(min-width: 1100px)" 
+			srcset="images/dr-kucovic.png">
+		<source 
+			media="(min-width: 768px)" 
+			srcset="images/dr-kucovic.png">
+		<source 
+			media="(min-width: 450px)" 
+			srcset="images/dr-kucovic.png">
+		<img src="images/dr-kucovic.png" alt="doki-vet galerija" class="img-responsive">
+		
+	</picture>
 </div><!-- END #blog -->
